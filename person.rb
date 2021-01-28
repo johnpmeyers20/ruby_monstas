37be88db24c3d13ec8e0216ddecb0de689f7b4fe
JP Meyers <johnpmeyers20@yahoo.com>
2021-01-27 20:52:41 -0500

    finished Attribute Writers, State and Behavior for our class Person

--- person.rb
+++ person.rb
@@ -6,8 +6,15 @@ class Person
   def name
     @name
   end
+
+  def password=(password)
+    @password = password
+  end
 end
 
 person = Person.new("Ada")
+person.password=("cowabunga123")
+person.password = "whatNow?"
 
 puts Person.new("JP").name
+puts person.password
